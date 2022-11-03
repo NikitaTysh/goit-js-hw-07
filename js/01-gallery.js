d5ae152f831010a8474972a983e9bb46df38ffc8
NikitaTysh <nikita.tyshchenko7@gmail.com>
2022-11-03 23:20:18 +0100

    main

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+const gallery = document.querySelector(".gallery");
+const elToAdd = galleryItems
+  .map(
+    (el) => `<div class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+      <img
+        class="gallery__image"
+        src="${el.preview}"
+        data-source="large-image.jpg"
+        alt="${el.description}"
+      />
+    </a>
+  </div>`
+  )
+  .join("");
+gallery.insertAdjacentHTML("beforeend", elToAdd);
+gallery.addEventListener("click", func);
+let attForModal;
+function func(evt) {
+  evt.preventDefault();
+  
+  galleryItems.forEach((el) => {
+    if (el.preview === evt.target.attributes.src.value) {
+      attForModal = el.original;
+    }
+  });
+console.log(attForModal);
+const instance = basicLightbox.create(
+  `<div class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+      <img
+        class="gallery__image"
+          data-source="large-image.jpg"
+          src = "${attForModal}"
+      />
+    </a>
+  </div>`
+)
+instance.show()
+}
 
-console.log(galleryItems);
+
